9ed80f0fecbfb08881d3bb3fe69f1b6cdfadf558
mughees605 <mughees605@gmail.com>
2017-08-11 18:19:47 +0500

    finalized scales

--- _04Scales/main.js
+++ _04Scales/main.js
@@ -1,10 +1,14 @@
-var dataArray = [20, 40, 60];
+var dataArray = [20, 40, 60, 70, 80];
 var width = 500;
 var height = 500;
 
 var widthScale = d3.scaleLinear()
-    .domain([0, 60])
+    .domain([0, 80])
     .range([0, width])
+
+var color = d3.scaleLinear()
+     .domain([0,80])
+     .range(["red","blue"])
     
 var canvas = d3.select("body")
     .append('svg')
@@ -17,4 +21,5 @@ var bars = canvas.selectAll("rect")
     .append("rect")
     .attr("width", (d) => {return widthScale(d)})
     .attr("height", 50)
+    .attr("fill",(d)=> color(d))
     .attr("y", (d, i) => { return i * 100 })
