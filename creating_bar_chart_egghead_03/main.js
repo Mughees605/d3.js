994911100084db73de3a4fbda208fadd1810e71c
mughees605 <mughees605@gmail.com>
2017-08-13 10:34:31 -0700

    created bar chart 01

--- creating_bar_chart_egghead_03/main.js
+++ creating_bar_chart_egghead_03/main.js
@@ -1,22 +1,36 @@
-var margin = { top: 0, right: 0, bottom: 25, left: 25 };
+var margin = { top: 10, right: 0, bottom: 40, left: 40 };
 var width = 425 - margin.right - margin.left;
 var height = 625 - margin.top - margin.bottom;
 
 var svg = d3.select('.chart')
     .append('svg')
     .attr('width', width + margin.right + margin.left)
-    .attr('height', width + margin.top + margin.bottom)
+    .attr('height', height + margin.top + margin.bottom)
     .append('g')
     .attr('transform', `translate(${margin.left} , ${margin.top})`)
 svg.append('rect')
-    .attr('width', width / 2)
+    .attr('width', width)
     .attr('height', height)
     .style('fill', "lightblue")
     .style('stroke', "green ")
+var yScale = d3.scaleLinear()
+.domain([0,6])
+.range([height, 0])
 
-svg.append('rect')
-    .attr('x', width/2)
-    .attr('width', width / 2)
-    .attr('height', height)
-    .style('fill', "lightblue")
-    .style('stroke', "green ")
+var yAxis = d3.axisLeft(yScale).ticks(6);
+svg.call(yAxis) 
+
+let xScale = d3.scaleLinear()
+.domain([0,100])
+.range([0,width])
+
+var xAxis = d3.axisBottom(xScale)
+svg.append('g')
+ .attr('transform','translate(0,'+height+')')
+.call(xAxis)
+// svg.append('rect')
+//     .attr('x', width/2)
+//     .attr('width', width / 2)
+//     .attr('height', height)
+//     .style('fill', "lightblue")
+//     .style('stroke', "green ")
