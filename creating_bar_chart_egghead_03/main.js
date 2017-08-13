8fa18f290cf73a8dd5e64fd7e5a9b8326a409ed6
mughees605 <mughees605@gmail.com>
2017-08-13 11:55:35 -0700

    completed axis of chart

--- creating_bar_chart_egghead_03/main.js
+++ creating_bar_chart_egghead_03/main.js
@@ -1,6 +1,6 @@
-var margin = { top: 10, right: 0, bottom: 40, left: 40 };
-var width = 425 - margin.right - margin.left;
-var height = 625 - margin.top - margin.bottom;
+var margin = { top: 10, right: 10, bottom: 40, left: 40 };
+var width = 400 - margin.right - margin.left;
+var height = 600 - margin.top - margin.bottom;
 
 var svg = d3.select('.chart')
     .append('svg')
@@ -8,23 +8,25 @@ var svg = d3.select('.chart')
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
     .attr('transform', `translate(${margin.left} , ${margin.top})`)
+
+
 svg.append('rect')
     .attr('width', width)
     .attr('height', height)
     .style('fill', "lightblue")
     .style('stroke', "green ")
 var yScale = d3.scaleLinear()
-.domain([0,6])
+.domain([0,10])
 .range([height, 0])
 
-var yAxis = d3.axisLeft(yScale).ticks(6);
+var yAxis = d3.axisLeft(yScale);
 svg.call(yAxis) 
 
 let xScale = d3.scaleLinear()
 .domain([0,100])
 .range([0,width])
 
-var xAxis = d3.axisBottom(xScale)
+var xAxis = d3.axisBottom(xScale).ticks(5)
 svg.append('g')
  .attr('transform','translate(0,'+height+')')
 .call(xAxis)
