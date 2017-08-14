c54817d7dd80504ce8c2068fb4ec890492e3ed74
mughees605 <mughees605@gmail.com>
2017-08-14 02:48:14 -0700

    completed responsive chart

--- responsive_view_box_egghead_04/main.js
+++ responsive_view_box_egghead_04/main.js
@@ -6,6 +6,7 @@ var svg = d3.select('.chart')
     .append('svg')
     .attr('width', width + margin.right + margin.left)
     .attr('height', height + margin.top + margin.bottom)
+    .call(responsivefy)
     .append('g')
     .attr('transform', `translate(${margin.left} , ${margin.top})`)
 
