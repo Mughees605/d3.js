420a045a02a62c6358a863cfd82b1b80aef6e109
mughees605 <mughees605@gmail.com>
2017-08-14 11:59:48 -0700

    created circles data

--- egghead-d3v4/src/app.js
+++ egghead-d3v4/src/app.js
@@ -13,44 +13,33 @@ d3.json('./data/data.json', function (err, data) {
   var yScale = d3.scaleLinear()
     .domain(d3.extent(data, d => d.expectancy))
     .range([height, 0])
-
+    .nice()
   var yAxis = d3.axisLeft(yScale);
   svg.call(yAxis)
 
   let xScale = d3.scaleLinear()
     .domain(d3.extent(data, d => d.cost))
     .range([0, width])
+    .nice()
   var xAxis = d3.axisBottom(xScale)
+    .ticks(5)
 
   svg.append('g')
     .attr('transform', 'translate(0,' + height + ')')
     .call(xAxis)
+
+  let rScale = d3.scaleSqrt()
+    .domain([0, d3.max(data, d => d.population)])
+    .range([0, 40])
+
+  svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', d => xScale(d.cost))
+    .attr('cy', d => yScale(d.expectancy))
+    .attr('r', d => rScale(d => d.population))
+    .style('fill', 'steelblue')
 })
 
 
-function responsivefy(svg) {
-  // get container + svg aspect ratio
-  var container = d3.select(svg.node().parentNode),
-    width = parseInt(svg.style("width")),
-    height = parseInt(svg.style("height")),
-    aspect = width / height;
-
-  // add viewBox and preserveAspectRatio properties,
-  // and call resize so that svg resizes on inital page load
-  svg.attr("viewBox", "0 0 " + width + " " + height)
-    .attr("preserveAspectRatio", "xMinYMid")
-    .call(resize);
-
-  // to register multiple listeners for same event type,
-  // you need to add namespace, i.e., 'click.foo'
-  // necessary if you call invoke this function for multiple svgs
-  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
-  d3.select(window).on("resize." + container.attr("id"), resize);
-
-  // get width of container and resize svg to fit it
-  function resize() {
-    var targetWidth = parseInt(container.style("width"));
-    svg.attr("width", targetWidth);
-    svg.attr("height", Math.round(targetWidth / aspect));
-  }
-}
